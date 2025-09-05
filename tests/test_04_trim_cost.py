014c4a0c6ea0674926f1cc0e54d524ef497addef
Chiranth A M <chiranth.ajjamanemanohar@ucdenver.edu>
2025-09-05 16:22:37 -0600

    updated typo

--- tests/test_04_trim_cost.py
+++ tests/test_04_trim_cost.py
@@ -27,13 +27,26 @@ class TestTrimCost(unittest.TestCase): #change class name
     
         trim_cost() #changed function call
         output = self.original_stdout.getvalue().strip() 
-        expected_output = "Perimeter of the box = 30.0\nNumber of trims you need to buy = 3\nTotal cost of the trims = 5.64\nThe amount of trims lost = 0.5\nThe cost lost by wasting the strp = 0.94" # change this
+        expected_output = "Perimeter of the box = 30.0\nNumber of trims you need to buy = 3\nTotal cost of the trims = 5.64\nThe amount of trims lost = 0.5\nThe cost lost by wasting the trim = 0.94\n" # change this
 
         self.assertEqual(
             output,
             expected_output,
             msg=f"\n❌ Wrong output.\n Please Check the calculation and the format of the display'"
         )
+    
+    # @patch('builtins.input', side_effect =['10.8', '55.8'])  # Mock user input
+    # def tests_trim_cost(self,mock_input): #change name
+
+    #     trim_cost() #changed function call
+    #     output = self.original_stdout.getvalue().strip() 
+    #     expected_output = "Perimeter of the box = 133.2\nNumber of trims you need to buy = 12\nTotal cost of the trims = 22.56\nThe amount of trims lost = 0.9000000000000004\nThe cost lost by wasting the trim = 1.6920000000000006\n" # change this
+
+    #     self.assertEqual(
+    #         output,
+    #         expected_output,
+    #         msg=f"\n❌ Wrong output.\n Please Check the calculation and the format of the display'"
+    #     )
 
 
 if __name__ == '__main__': 
